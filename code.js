5f7374085f8bd5b8356a197854aeabdfe15b975d
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-10-05 18:34:50 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,17 +1,15 @@
 function fib(n) {
-    let values = [];
-    return fibStep(n,values);
-}
-
-function fibStep(n,values) {
+    let values = []
     if (n===0) {
-        return [0]; 
+        values = [0];
+        return values;
     }
-    else if (n===1) {
-        return [0,1];
+    values = [0,1]; 
+    for (i=2; i<=n; i++) {
+        values.push(values[i-1] + values[i-2]);
     }
-    else {
-        values[n] = fibStep(n-1) + fib(n-2);
-    }
+    
     return values;
 }
+
+console.log(fib(3))
