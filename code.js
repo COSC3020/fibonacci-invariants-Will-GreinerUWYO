552d5764b69c14b6d58a452c2fa5fd5d89066c9d
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-10-05 18:16:01 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function fib(n) {
-    return n;
+    if (n===0) {
+        return [0]; 
+    }
+    else if (n===1) {
+        return [0,1];
+    }
+    else {
+        let values = [];
+        values[n] = fib(n-1) + fib(n-2);
+    }
+    return values;
 }
